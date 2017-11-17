f512fc711fd72b5e6e8d83883da969747289d850
Axel Siebenborn <axel.siebenborn@sap.com>
2017-11-17 14:24:27 +0100

    Add setup_repo.sh for an initial setup of the repository.
    The .hg directory is added to the git repository in an extra branch
    This way we don't loose that information for updating.

---
+++ setup_repo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#HG_HOST="hg.openjdk.java.net"
+#HG_PATH="jdk/jdk"
+HG_HOST="bitbucket.org"
+HG_PATH="axel7born/mercurial2git"
+
+
+
+GIT_REPO="http://${GIT_USER}:${GIT_PASSWORD}@github.com/axel7born/mercurial2git"
+
+git hg clone "http://$HG_HOST/$HG_PATH"
+
+REPO_PATH="$(basename $HG_PATH)"
+cd $REPO_PATH
+
+git remote add origin $GIT_REPO
+
+# save .hg dir
+git checkout -b hg-info
+rm .git
+mv .hg/git .git
+git add -f .hg
+git commit -m "Add .hg dir"
+git push origin hg-info
+
+git checkout master
+git checkout -b "$HG_PATH"
+git push origin "$HG_PATH"
--- update_repo.sh
+++ update_repo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 #HG_HOST="hg.openjdk.java.net"
 #HG_PATH="jdk/jdk"
@@ -9,12 +10,30 @@ HG_PATH="axel7born/mercurial2git"
 
 GIT_REPO="http://${GIT_USER}:${GIT_PASSWORD}@github.com/axel7born/mercurial2git"
 
-git hg clone "http://$HG_HOST/$HG_PATH"
+git clone $GIT_REPO
 
-REPO_PATH="$(basename $HG_PATH)"
+REPO_PATH="$(basename $GIT_REPO)"
 cd $REPO_PATH
 
-git remote add origin $GIT_REPO
+#get hg info
+git checkout hg-info
+mv .git .hg/git
+ln -s .hg/git .git
+
+git rm --cached -r .hg
+git commit -m "remove .hg from git"
+
+git checkout master
+git hg pull
+
 git checkout -b "$HG_PATH"
+git rebase master
 
 git push origin "$HG_PATH"
+
+git checkout hg-info
+rm .git
+mv .hg/git .git
+git add -f .hg
+git commit -m "Add .hg dir"
+git push origin hg-info
