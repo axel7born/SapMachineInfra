2b804771ebfcff880b27c49af18504cf39db8975
Axel Siebenborn <axel.siebenborn@sap.com>
2017-11-23 14:21:30 +0100

    Install github-release first.

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
+bunzip2 linux-amd64-github-release.tar.bz2
+tar xvf linux-amd64-github-release.tar
+export PATH=$PWD/bin/linux/amd64/:$PATH
+
 #check if release exists
 
 if output=$(github-release info -u SAP -r SapMachine -t $TAG);then
@@ -20,13 +25,6 @@ bash configure --with-boot-jdk="/openjdk/build/jdk-9+181/"
 make images
 
 tar zcvf SapMachine.tgz build/linux-x86_64-normal-server-release/images/jdk/
-
-wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
-
-bunzip2 linux-amd64-github-release.tar.bz2
-tar xvf linux-amd64-github-release.tar
-
-export PATH=$PWD/bin/linux/amd64/:$PATH
 
 if [ -z $GITHUB_TOKEN ]; then
   echo "FAILURE: GITHUB_TOKEN not set"
