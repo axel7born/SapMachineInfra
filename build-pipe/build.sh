fc9fafe331889d4f132e9e70cf90d09b3fedbb3d
Axel Siebenborn <axel.siebenborn@sap.com>
2017-11-28 14:23:26 +0100

    Add version-opt field to version string to configure.

--- build-pipe/build.sh
+++ build-pipe/build.sh
@@ -11,9 +11,9 @@ cd SapMachine
 
 if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == jdk-* ]]; then
   git checkout $GIT_TAG_NAME
-  bash ./configure --with-boot-jdk=$BOOT_JDK --with-version-string=${GIT_TAG_NAME: 4}
+  bash ./configure --with-boot-jdk=$BOOT_JDK --with-version-string=${GIT_TAG_NAME: 4} --with-version-opt="sapmachine"
 else
-  bash ./configure --with-boot-jdk=$BOOT_JDK
+  bash ./configure --with-boot-jdk=$BOOT_JDK --with-version-opt="sapmachine"
 fi
 
 make JOBS=12 images
